e71261540448cb7085d71842008cf5a28fd2b07a
Jordi Bayer <jordi.be.1992@gmail.com>
2023-10-15 18:40:51 +0200

    16.11

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,7 +5,13 @@ function Options({ question, dispatch, answer }) {
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
           key={option}
           disabled={hasAnswered}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}>
